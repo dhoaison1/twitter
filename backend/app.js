a69fce0225f8943df8618dfbba032e92ddd0e700
Doan Son <Doan Son@sondh.imt-soft.com>
2018-12-02 20:58:02 +0700

    Initialize Project

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${ PORT }`);
+});
